4cfac7e0d43fce82a18ec22f77ef33f08b49ba02
[dyad] <tamangelarivera@gmail.com>
2025-10-03 22:07:02 +0800

    [dyad] Updated sidebar icons with orange palette - wrote 1 file(s)

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -7,30 +7,30 @@ const Sidebar = () => {
     <div className="w-16 bg-[#FFFFFF] border-r border-gray-200 flex flex-col items-center py-4 space-y-6">
       {/* Navigation Icons */}
       <div className="flex flex-col items-center space-y-6">
-        <button className="p-3 rounded-full hover:bg-gray-100">
-          <Home className="w-6 h-6 text-gray-600" />
+        <button className="p-3 rounded-full hover:bg-[#FFF5EB]">
+          <Home className="w-6 h-6 text-[#FF8D21]" />
         </button>
-        <button className="p-3 rounded-full hover:bg-gray-100">
-          <Search className="w-6 h-6 text-gray-600" />
+        <button className="p-3 rounded-full hover:bg-[#FFF5EB]">
+          <Search className="w-6 h-6 text-[#FFA652]" />
         </button>
         <button className="p-3 rounded-full bg-[#FFF5EB]">
           <Plus className="w-6 h-6 text-[#FF7B00]" />
         </button>
-        <button className="p-3 rounded-full hover:bg-gray-100">
-          <Heart className="w-6 h-6 text-gray-600" />
+        <button className="p-3 rounded-full hover:bg-[#FFF5EB]">
+          <Heart className="w-6 h-6 text-[#FFB76B]" />
         </button>
-        <button className="p-3 rounded-full hover:bg-gray-100">
-          <User className="w-6 h-6 text-gray-600" />
+        <button className="p-3 rounded-full hover:bg-[#FFF5EB]">
+          <User className="w-6 h-6 text-[#FFCD90]" />
         </button>
       </div>
       
       {/* Bottom Icons */}
       <div className="mt-auto flex flex-col items-center space-y-6">
-        <button className="p-3 rounded-full hover:bg-gray-100">
-          <Bookmark className="w-6 h-6 text-gray-600" />
+        <button className="p-3 rounded-full hover:bg-[#FFF5EB]">
+          <Bookmark className="w-6 h-6 text-[#FFA652]" />
         </button>
-        <button className="p-3 rounded-full hover:bg-gray-100">
-          <Menu className="w-6 h-6 text-gray-600" />
+        <button className="p-3 rounded-full hover:bg-[#FFF5EB]">
+          <Menu className="w-6 h-6 text-[#FF8D21]" />
         </button>
       </div>
     </div>
